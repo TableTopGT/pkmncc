b407589bca0f5b4be434dd3f87baf3105d055712
Nanley Chery <nanleychery@gmail.com>
2012-02-28 11:31:04 -0500

    Add image for start screen

--- PokemonCardColMain/src/org/tabletop/pokemon/Game.java
+++ PokemonCardColMain/src/org/tabletop/pokemon/Game.java
@@ -7,7 +7,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Texture;
 import java.lang.String;
+
 
 public class Game implements ApplicationListener {
 	public static int SCREEN_WIDTH = 1280;
@@ -15,9 +17,10 @@ public class Game implements ApplicationListener {
 	private static String buttonOne = "startButton";
 	
 	Stage mainMenu;
-	SpriteBatch menuBatch;
+	SpriteBatch menuBatch, batch;
 	Actor startButton, exitButton;
 	Music introMusic;
+	Texture startscreen;
 	
 	@Override
 	public void create() {
@@ -26,6 +29,10 @@ public class Game implements ApplicationListener {
 		SCREEN_HEIGHT = Gdx.graphics.getHeight();
 		menuBatch = new SpriteBatch();
 		mainMenu = new Stage(SCREEN_WIDTH, SCREEN_HEIGHT, false, menuBatch);
+		
+		batch = new SpriteBatch();
+		startscreen =  new Texture(Gdx.files.internal("images/badlogic.jpg"));
+
 		
 		introMusic = Gdx.audio.newMusic(Gdx.files.getFileHandle("music/title.mp3", FileType.Internal));
 		introMusic.play();
@@ -40,7 +47,9 @@ public class Game implements ApplicationListener {
 	@Override
 	public void render() {
 		// TODO Auto-generated method stub
-
+		batch.begin();
+		batch.draw(startscreen, 10, 10);
+		batch.end();
 	}
 
 	@Override
