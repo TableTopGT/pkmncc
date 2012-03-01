8f411eb6064e51cd03ad82432996124bbacfe424
Pratima Narlajarla <nvpratima23@gmail.com>
2012-03-01 09:37:16 -0500

    March 1, 9:35 Added -RFID Listener Class

---
+++ PokemonCardColMain/src/org/tabletop/pokemon/RFIDListener.java
@@ -0,0 +1,30 @@
+package org.tabletop.pokemon;
+
+public class RFIDListener {
+	public static String RFIDTag;
+	public String getTag() {
+		
+		// Reads of the output of the RFID reader
+		RFIDTag = this.getTag();
+		}
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public Pokemon getCard (String RFIDTag){
+		// Takes in the string, compares it with known values and returns the card
+		if (RFIDTag == "O11111111")
+			return new Charizard();
+		else if (RFIDTag == "2222222222")
+			return new Charmander();
+		else 
+			return null;
+		}
+
+}
