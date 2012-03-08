2ded0e7b2f81fad8e618789325cf6af7b9df2a1a
Katie.Runyan <katierunyan@gatech.edu>
2012-03-08 12:00:30 -0500

    Edited Battle class -created Battle constructor -created continueBattle method -created player1turn1 method -created anyTurn method

--- PokeCardGame/src/org/tabletop/pkmncc/Battle.java
+++ PokeCardGame/src/org/tabletop/pkmncc/Battle.java
@@ -1,7 +1,36 @@
 package org.tabletop.pkmncc;
 
 public class Battle {
+	public Player player1;
+	public Player player2;
+	public Player winner = null;
 	
 	
+	public Battle(){
+		//instantiate players
+		player1 = new Player();
+		player2 = new Player(player1);
+		player1.setOpponent(player2);
+	}
+	
+	public Player continueBattle(){
+		player1turn1();
+		while (winner == null){
+			anyTurn(player1);
+			//make sure player 1 didn't win before completing the loop
+			if (winner == null){
+			anyTurn(player2);
+			}
+		}
+		return winner;
+	}
+	
+	public void player1turn1(){
+		
+	}
+	
+	public void anyTurn(Player thisPlayer){
+		
+	}
 	
 }
