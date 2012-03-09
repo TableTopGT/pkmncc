a369b3cbd90885ce5834a7526a352e8be54faae0
Katie.Runyan <katierunyan@gatech.edu>
2012-03-09 00:38:21 -0500

    Edited Battle Class -declared battle buttons -edited anyTurn method
    Edited RFID class
    -created cardType variable--card type gets written to this variable
    every time a card is swiped and the getType() method returns what type
    the card is

--- PokeCardGame/src/org/tabletop/pkmncc/Battle.java
+++ PokeCardGame/src/org/tabletop/pkmncc/Battle.java
@@ -1,10 +1,27 @@
 package org.tabletop.pkmncc;
+import org.tabletop.pkmncc.RFIDListener.CardType;
 
-public class Battle {
+import android.app.Activity;
+import android.view.View;
+
+public class Battle extends Activity {
 	public Player player1;
 	public Player player2;
 	public Player winner = null;
 	public RFIDListener cardListener;
+	public Card newCard = null;
+	public CardType newCardType;
+	public boolean endTurn;
+	
+	//set up buttons
+	public View p1action1;
+	public View p1action2;
+	public View p1switch;
+	public View p1endTurn;
+	public View p2action1;
+	public View p2action2;
+	public View p2switch;
+	public View p2endTurn;
 	
 	
 	public Battle(){
@@ -12,6 +29,12 @@ public class Battle {
 		player1 = new Player();
 		player2 = new Player(player1);
 		player1.setOpponent(player2);
+		
+		cardListener = new RFIDListener();
+		
+		endTurn = false;
+		
+		//draw end turn and switch buttons
 	}
 	
 	public Player continueBattle(){
@@ -27,11 +50,55 @@ public class Battle {
 	}
 	
 	public void player1turn1(){
-		
+		//first move must be to add an active pokemon
+		while (newCard==null){
+		newCard = cardListener.getCard();
+		newCardType = cardListener.getCardType();
+		}
+		if (newCardType == CardType.POKEMONCARD){
+			player1.addCard(newCard);
+			newCard = null;
+			newCardType=null;
+		}
+		//now player can play either more pokemon or energy until turn is ended
+		while (endTurn==false){
+			newCard = cardListener.getCard();
+			newCardType = cardListener.getCardType();
+			if (newCardType == CardType.POKEMONCARD || newCardType == CardType.POKEMONCARD){
+				player1.addCard(newCard);
+				newCard = null;
+				newCardType = null;
+			}
+		}
+				
 	}
 	
 	public void anyTurn(Player thisPlayer){
+		endTurn = false;
+		//draw buttons and setOnClickListener just for current player's buttons
+		if (thisPlayer == player1){
+			
+		}
+		else if (thisPlayer == player2){
+			
+		}
 		
+		while (endTurn = false){
+			//Option 1: swipe a card
+			newCard = cardListener.getCard();
+			thisPlayer.addCard(newCard);
+			newCard = null;
+			//update screed
+			
+			//Option 2: switch active and benched pokemon
+			
+						
+			//Option 3: attack your opponent
+			
+			
+			//Option 4: end your turn (without attack)
+		}
 	}
+	
 	
 }
--- PokeCardGame/src/org/tabletop/pkmncc/RFIDListener.java
+++ PokeCardGame/src/org/tabletop/pkmncc/RFIDListener.java
@@ -8,6 +8,8 @@ public class RFIDListener {
 	
 	public String RFIDTag;
 	public Card newCard;
+	public static enum CardType {POKEMONCARD, ENERGYCARD, TRAINERCARD};
+	public CardType thisCardType;
 		
 	/*public String getTag() {
 		
@@ -21,58 +23,79 @@ public class RFIDListener {
 		//equal to RFIDTag				
 		
 		// Takes in the string, compares it with known values and returns the card
-		if (RFIDTag == "O11111110")
+		if (RFIDTag == "O11111110"){
 			newCard = new Charizard();
-		else if (RFIDTag == "0222222220")
+			thisCardType = CardType.POKEMONCARD;
+		}
+		else if (RFIDTag == "0222222220"){
 			newCard = new Charmander();
+			thisCardType = CardType.POKEMONCARD;
+		}
 		else if (RFIDTag == "03333333330"){
 			newCard = new Charmeleon();
+			thisCardType = CardType.POKEMONCARD;
 		}
 		else if (RFIDTag == "0444444440"){
 			newCard = new Energy();
 			((Energy) newCard).setType(EnergyType.COLORLESS);
+			thisCardType = CardType.ENERGYCARD;
 		}
 		else if (RFIDTag == "0555555550"){
 			newCard = new Energy();
 			((Energy) newCard).setType(EnergyType.DARKNESS);
+			thisCardType = CardType.ENERGYCARD;
 		}
 		else if (RFIDTag == "0666666660"){
 			newCard = new Energy();
 			((Energy) newCard).setType(EnergyType.FIGHTING);
+			thisCardType = CardType.ENERGYCARD;
 		}
 		else if (RFIDTag == "0777777770"){
 			newCard = new Energy();
 			((Energy) newCard).setType(EnergyType.FIRE);
+			thisCardType = CardType.ENERGYCARD;
 		}
 		else if (RFIDTag == "0888888880"){
 			newCard = new Energy();
 			((Energy) newCard).setType(EnergyType.GRASS);
+			thisCardType = CardType.ENERGYCARD;
 		}
 		else if (RFIDTag == "0999999990"){
 			newCard = new Energy();
 			((Energy) newCard).setType(EnergyType.LIGHTNING);
+			thisCardType = CardType.ENERGYCARD;
 		}
 		else if (RFIDTag == "0121212120"){
 			newCard = new Energy();
 			((Energy) newCard).setType(EnergyType.METAL);
+			thisCardType = CardType.ENERGYCARD;
 		}
 		else if (RFIDTag == "0131313130"){
 			newCard = new Energy();
 			((Energy) newCard).setType(EnergyType.WATER);
+			thisCardType = CardType.ENERGYCARD;
 		}
 		else if (RFIDTag == "0141414140"){
 			newCard = new Trainer(TrainerType.ENERGYREMOVAL);
+			thisCardType = CardType.ENERGYCARD;
 		}
 		else if (RFIDTag == "0161616160"){
 			newCard = new Trainer (TrainerType.FULLHEAL);
+			thisCardType = CardType.ENERGYCARD;
 		}
 		else if (RFIDTag == "0171717170"){
 			newCard = new Trainer (TrainerType.POTION);
+			thisCardType = CardType.ENERGYCARD;
 		}
 		else {
-			return null;
+			newCard = null;
+			thisCardType = null;
 		}
 	
 		return (Card) newCard;
+	}
+	
+	public CardType getCardType(){
+		return thisCardType;
 	}
 }
