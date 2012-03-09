50aa3ac3cd382bfe080c0b8e06d56765d98cb735
Katie.Runyan <katierunyan@gatech.edu>
2012-03-09 12:12:11 -0500

    Manually updated my version of the Energy class (to match Nanley's last version)

--- PokeCardGame/src/org/tabletop/pkmncc/Energy.java
+++ PokeCardGame/src/org/tabletop/pkmncc/Energy.java
@@ -5,14 +5,18 @@ public class Energy extends Card {
 	public static enum EnergyType {GRASS, FIRE, WATER, LIGHTNING, PSYCHIC, FIGHTING, DARKNESS, METAL, COLORLESS};
 	
 	private EnergyType type;
-
+	
+	public Energy(EnergyType type) {
+		this.type = type;
+	}
+	
 	public EnergyType getType() {
 		return type;
 	}
 
 	// setter returns this for chaining
-	public Energy setType(EnergyType aType) {	
-		type = aType;
+	public Energy setType(EnergyType type) {	
+		this.type = type;
 		return this;
 	}
 
