f0b5eaa83904c3e1a30900c569b702198f746eb1
Nanley Chery <nanleychery@gmail.com>
2012-03-10 13:46:36 -0500

    actually remove other files this time

--- PokemonCanvasColDesktop/src/org/tabletop/pokemon/DesktopGame.java
+++
@@ -1,9 +0,0 @@
-package org.tabletop.pokemon;
-
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-
-public class DesktopGame {
-        public static void main (String[] args) {
-                new LwjglApplication(new Game(), "Main_Game", 1280, 800, false);
-        }
-}
